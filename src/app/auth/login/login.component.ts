0c4a2715d88e956d361af1b9553b6e65984b0fbb
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-05 10:33:08 +0800

    Extract form values

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -10,6 +10,8 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
   onSubmit(formData: NgForm) {
-    console.log(formData);
+    const enteredEmail = formData.form.value.email;
+    const enteredPassword = formData.form.value.password;
+    //console.log(formData);
   }
 }
