7050376c59a1ef7068429d74895c11e0c6dc8804
Stephanie Micah Cahutay <stephanie.m.cahutay@accenture.com>
2024-08-05 10:45:41 +0800

    Add return if form is invalid in login ts

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -10,6 +10,10 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
   onSubmit(formData: NgForm) {
+    if (formData.form.invalid) {
+      return;
+    }
+    
     const enteredEmail = formData.form.value.email;
     const enteredPassword = formData.form.value.password;
 
